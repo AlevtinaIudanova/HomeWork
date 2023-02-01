cd26713cabb203f5ea17788a97ac5b56c8b3e392
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-01 15:25:40 +0100

    5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Показать четные числа от 1 до N*/
+
+int N;
+
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int index = 2;
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index} ");
+    }
+    index += 2;
+}
