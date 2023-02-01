f1ee83cea22b99d6b1fc5f990892009e9aee3172
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-01 15:15:10 +0100

    1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*По двум заданным числам проверять является ли первое квадратом второго*/
+Console.WriteLine("Введите первое число ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Является ");
+}
+
+else
+{
+    Console.WriteLine("Не является ");
+}
