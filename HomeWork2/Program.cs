57cfc06d7df0bc720e72455facc9a2882ca97bc8
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-01 15:20:52 +0100

    2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+/*По заданному номеру дня недели вывести его название*/
+
+Console.WriteLine("Add number: ");
+string number = Console.ReadLine();
+if (number == "1")
+{
+    Console.WriteLine("Monday");
+}
+
+if (number == "2")
+{
+    Console.WriteLine("Tuesday");
+}
+
+if (number == "3")
+{
+    Console.WriteLine("Wednesday");
+}
+
+if (number == "4")
+{
+    Console.WriteLine("Thursday");
+}
+
+if (number == "5")
+{
+    Console.WriteLine("Thursday");
+}
+
+if (number == "6")
+{
+    Console.WriteLine("Saturday");
+}
+
+if (number == "7")
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("error");
+}
+
