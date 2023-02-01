a9c509a6db12dc7cdf1832a0004890f0a754c6bc
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-01 15:26:35 +0100

    6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Показать последнюю цифру трёхзначного числа*/
+
+Console.WriteLine("Введите трехзначное число ");
+double number = double.Parse(Console.ReadLine());
+
+string str_num = number.ToString();
+int a = int.Parse(str_num[0].ToString());
+int b = int.Parse(str_num[0].ToString());
+int c = int.Parse(str_num[0].ToString());
+
+Console.WriteLine(str_num[2]);
+
+
+
+
