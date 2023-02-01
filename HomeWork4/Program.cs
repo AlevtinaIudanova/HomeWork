5138663ab1082b99719bdae5009a514693e32df5
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-01 15:24:49 +0100

    4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Выяснить является ли число чётным*/
+
+Console.WriteLine("Введите первое число ");
+double number = double.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write("Число четное ");
+}
+else
+{
+    Console.Write("Число не четное");
+}
+
