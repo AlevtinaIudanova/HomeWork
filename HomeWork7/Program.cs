e1cd475233656fed1128f187c0d35db8d19d2ca0
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-02 11:53:45 +0100

    new commit

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Показать вторую цифру трёхзначного числа*/
 
 Console.WriteLine("Введите трехзначное число ");
-double number = double.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 
 string str_num = number.ToString();
 int a = int.Parse(str_num[0].ToString());
