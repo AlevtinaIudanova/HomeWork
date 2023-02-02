efd578dadb4e7962f15144b4cd10e958db1c311e
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-02 14:17:17 +0100

    9

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Удалить вторую цифру трёхзначного числа*/
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+/*int num = new Random().Next(100, 1000);*/
+
+int a = number / 100;
+int b = number % 10;
+
+Console.Write($"{number} -> {a}{b} ");
