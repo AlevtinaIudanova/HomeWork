488bc04c53b55442c4ec7eae5f34b7940570bcc4
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-02 12:49:00 +0100

    HomeWork8

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Дано число из отрезка [10, 99]. Показать наибольшую цифру числа*/
+
+Console.WriteLine("Введите число из отрезка [10, 99]: ");
+int number = int.Parse(Console.ReadLine());
+
+string str_num = number.ToString();
+int a = int.Parse(str_num[0].ToString());
+int b = int.Parse(str_num[1].ToString());
+
+if (a > b)
+{
+    Console.Write(a);
+}
+else
+{
+    Console.Write(b);
+}
