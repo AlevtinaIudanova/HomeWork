e7ec54724a3fa4315e90fa715546150b6b413e09
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-04 09:51:21 +0100

    11

---
+++ HomeWork11/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Найти третью цифру числа или сообщить, что её нет*/
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+string str_num = number.ToString();
+int a = int.Parse(str_num[2].ToString());
+
+Console.WriteLine(str_num[2]);
+
