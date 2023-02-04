b24fec597daf7c937d08a00d0940eef8564196c9
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-04 09:36:35 +0100

    10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Выяснить, кратно ли число заданному, если нет, вывести остаток*/
+
+int a;
+int b;
+
+Console.Write("Введите число 1: ");
+int.TryParse(Console.ReadLine(), out a);
+Console.Write("Введите число 2: ");
+int.TryParse(Console.ReadLine(), out b);
+
+if (a % b == 0)
+{
+    Console.Write("Число кратно -> ");
+}
+else
+{
+    Console.Write($"Число не кратное -> остаток {a % b} ");
+}
+
+
